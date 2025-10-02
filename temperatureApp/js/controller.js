24137fe2485647012999c2ef1d93a0a1a549286f
GET Academy <terje@kolderup.net>
2025-10-02 10:28:05 +0200

    Update controller.js

--- temperatureApp/js/controller.js
+++ temperatureApp/js/controller.js
@@ -1,11 +1,17 @@
 function registerTemperature()
 {
+    // Flere måter å kopiere et objekt på
+    // 1: 
     const newRegistration = {
         temperature: model.viewState.registrationPage.temperature,
         date: model.viewState.registrationPage.date,
     };
-    model.viewState.registrationPage.temperature = '';
-    model.viewState.registrationPage.date = '';
+    // 2: 
+    //const newRegistration = structuredClone(model.viewState.registrationPage);  
+    // 3: 
+    // const newRegistration = {...model.viewState.registrationPage.temperature};
+    // model.viewState.registrationPage.temperature = '';
+    // model.viewState.registrationPage.date = '';
     model.temperatureRegistrations.push(newRegistration);
     model.app.currentPage = 'graph';
 
