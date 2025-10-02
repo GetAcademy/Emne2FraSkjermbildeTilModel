4a7c68d029db6932eef668b3303ed9d213715846
GET Academy <terje@kolderup.net>
2025-10-02 10:10:28 +0200

    x

---
+++ feedback1/model.js
@@ -0,0 +1,58 @@
+const model = {
+    app: {
+        currentPage: 'main',
+        currentUser: '',
+    },
+    viewState: {
+        registerActivity: {
+            title: '',
+            description: '',
+            category: '',
+            time: '',
+            difficulty: '',
+            place: '',
+            participants: '',
+            picture: '',
+        },
+        showActivity: {
+            stars: '',
+            comments: '',
+        },
+        sideBar: {
+            search: '',
+            category: '',
+            time: '',
+            difficulty: '',
+            rank: '',
+            place: '',
+            participants: '',
+        },
+    },
+    data: {
+        users: [
+            // favorites = id til dine favoritter
+            // activities = id til de du har laget
+            // husk annen måte å gjøre kommentar på
+            { userID: 1, name: 'Marie', img: '', favorites: [3], activitiesID: [1], },
+            { userID: 2, name: 'Bjarne', img: '', favorites: [1], activitiesID: [3], },
+        ],
+        activities: [
+            {
+                activityID: 1,
+                title: 'Klatring i fjell',
+                description: 'Ta en tur i fjellet og klatre opp en bratt bakke',
+                category: 'Klatring',
+                time: '1t30m',
+                difficulty: 'moderate',
+                place: 'Fjellet',
+                participants: null,
+                picture: '',
+                starAverage: null,
+                comments: [
+                    { userID: '', comment: '', stars: null, },
+                    { userID: '', comment: '', stars: null, },
+                ],
+            },
+        ]
+    },
+}
---
+++ feedback2/model.js
@@ -0,0 +1,70 @@
+const model = {
+    app: {
+        currentPage: 'homePage', //activityPage, profilePage
+    },
+
+    viewState: {
+        //viewState per side
+        homePage: {
+            filter: {
+                categories: ['Fjell'],
+                difficulty: [1, 2, 3, 4, 5],
+            },
+            categories: ['Vinter', 'Sommer', 'Høst', 'Vår'],
+            post: {
+                userName: 'sondre',
+                title: 'Gå på ski',
+                image: 'ski.png',
+                descriptions: 'ski ski ski',
+                favorites: true,
+                ratings: 3,
+                difficulty: 4,
+                comments: '',
+            },
+            search: '',
+        },
+        profilePage: {
+            profilePic: 'pfp.png',
+            userName: 'sondre',
+            favorites: ['Fisking'],
+            activity: ['Fisking', 'Kajakk', 'Alpin'],
+        },
+        activityPage: {
+            difficulty: 5,
+            estimatedTime: 6 + 'hrs',
+            descriptions: 'Hærlig tur! Ikke forvent noe fangst så ta med egen grillmat!',
+            ratings: 3,
+            img1: 'FishingRod.png',
+            img2: 'HappyGuy.png',
+            categories: ['Skogstur', 'Vinter', 'Sommer', 'Klatring', 'Skitur', 'Sykkeltur', 'Fjelltur'],
+        },
+        // logInPage
+        // registerPage
+    },
+
+    //data
+    users: [
+        {
+            userName: '',
+            password: '',
+            profilePic: '',
+            favorites: [''],
+            activity: [''],
+            followers: 0,
+        },
+    ],
+    posts: [
+        {
+            user: '',
+            img: '',
+            title: '',
+            description: '',
+            ratings: 0,
+            difficulty: 0,
+            favorites: false,
+            estimatedTime: 0 + 'hrs',
+            category: '',
+            comments: '',
+        }
+    ],
+};
